e81589e281b82f82f89a9b927d71dd35c2bc2372
Erick M. L. Pacheco <38138775+rckmath@users.noreply.github.com>
2020-09-07 14:07:23 -0300

    Updates

---
+++ precache-manifest.a41d92cdd3c0f748ef9303e622e2ae27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e31c45ae5a8ae49fa37cf8b60497446c",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "ac6585f792571006e5b1",
+    "url": "/me/static/css/main.a790c3ea.chunk.css"
+  },
+  {
+    "revision": "37cf377ce5c786afc8c7",
+    "url": "/me/static/js/2.14f287ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/me/static/js/2.14f287ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac6585f792571006e5b1",
+    "url": "/me/static/js/main.b806608c.chunk.js"
+  },
+  {
+    "revision": "be00c1860f5e70141225",
+    "url": "/me/static/js/runtime-main.de9a962f.js"
+  }
+]);
