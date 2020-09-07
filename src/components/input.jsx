c09367cd1900406b3b6168b8353940f574eb49d1
Erick M. L. Pacheco <38138775+rckmath@users.noreply.github.com>
2020-09-07 01:19:17 -0300

    Main interface started

---
+++ src/components/input.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () =>
+  <div class="form">
+    <form action="#">
+      <label for="input">C:\Users\erick{'>'}</label>
+      <input type="text" id="input" class="input"/>
+    </form>
+  </div>
