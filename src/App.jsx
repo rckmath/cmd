a6a86cfde23961def3073f738002e8dc5f45de0a
Erick M. L. Pacheco <38138775+rckmath@users.noreply.github.com>
2020-09-08 08:40:34 -0300

    Removed display; base will be develop in the future

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,10 @@ import React from 'react';
 import './App.css';
 import Input from './components/Input';
 
+
 export default (props) => (
   <div className="App">
-    <p>Portfolio [versão 0.1.0]<br/>
-      (c) 2020 Erick Matheus. Todos os direitos reservados.
-    </p>
+    <p>Portfolio [versão 0.1.0]<br/>(c) 2020 Erick Matheus. Todos os direitos reservados.</p>
     <Input/>
   </div>
 )
