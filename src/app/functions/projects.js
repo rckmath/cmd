89f0c2bf5989abec444320c33c2c5badb9f80214
erick_matheus <erick.lopes@mblabs.com.br>
2021-08-04 10:21:34 -0300

    feat: 2021 update done

--- src/app/functions/projects.js
+++ src/app/functions/projects.js
@@ -1,9 +1,9 @@
 const GITHUB_API_BASE_URL = 'https://api.github.com';
 
 export default async function () {
-  const repos = await fetch(`${GITHUB_API_BASE_URL}/users/rckmath/repos`, { method: 'get' })
+  const repositories = await fetch(`${GITHUB_API_BASE_URL}/users/rckmath/repos`, { method: 'get' })
     .then(res => res.json())
-    .catch((err) => { return 'Oops! Alguma coisa deu errado.' });
+    .catch((err) => { return `Oops! Alguma coisa deu errado. Detalhes do erro: ${err}`; })
   
-  return repos.filter((repo) => !repo.fork);
+  return repositories.filter((repo) => !repo.fork);
 }
